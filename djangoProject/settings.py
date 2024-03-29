67ff386c5d1ea749ae68f27195edf3e6806011e0
kaydurgu <mzhanbolot@gmail.com>
2024-03-29 10:31:55 +0300

    women app created ,models created registered, serializers created, womenlist checkedapi

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'women.apps.WomenConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
