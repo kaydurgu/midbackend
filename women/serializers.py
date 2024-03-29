a56dd6aea670253a945cd7da83cb439ef8cdaa9e
kaydurgu <mzhanbolot@gmail.com>
2024-03-29 18:16:05 +0300

    Serialized for model women model added , for WomenAPIVie get and post methods are added , post methods exception is added for required fileds

--- women/serializers.py
+++ women/serializers.py
@@ -1,9 +1,20 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
 
 from women.models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = '__all__'
+
+#class WomenModel:
+#    def __init__(self, title, content):
+#        self.title = title
+#        self.content = content
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField()
+    time_update = serializers.DateTimeField()
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
