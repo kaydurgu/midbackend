a09723d744f7e3e22dbdb250f5acb9154128017b
kaydurgu <mzhanbolot@gmail.com>
2024-03-30 12:25:02 +0300

    API client test ,API testcases

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -22,7 +22,7 @@ from .yasg import urlpatterns as doc_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api_schema', get_schema_view(title='Famous Women API'), name='api-schema'),
-    path("api/vi/womenlist/", WomenAPIList.as_view()),
+    path("api/vi/womenlist/", WomenAPIList.as_view(), name="womenlist"),
     path('api/vi/womenlist_update_data/<int:pk>/', WomenAPIUpdate.as_view()),
     path('api/vi/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
     path('api/vi/catergorylist/', CaregoryAPIView.as_view()),
--- women/tests.py
+++ women/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
+from django.urls import reverse
+from rest_framework import status
+from .views import WomenAPIList
+from .models import Women
+from rest_framework.test import APIClient
 
-# Create your tests here.
+#client = APIClient()
+#client.post('/notes/', {'title': 'new idea'}, format='json')
+class WomenTestCase(APITestCase):
+    def test_women(self):
+        response = self.client.get(reverse('womenlist'))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+class WomenListTest(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view =  WomenAPIList.as_view()
+        self.url = reverse('womenlist')
+    def test_women_list(self):
+        request = self.factory.get(self.url)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+       # self.assertEqual(response.data['count'], 0)
+       # self.assertEqual(response.data['results'], [])
+
+#class WomenAPI(APITestCase):
+#    def test_create_account(self):
+#        url = reverse('Women')
+#        data = {'name': 'DabApps'}
+#        response = self.client.post(url, data, format='json')
+#        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#        self.assertEqual(Women.objects.count(), 1)
+#        self.assertEqual(Women.objects.get().name, 'DabApps')
